f81e9dec47398c12abfe135d1ea328d5a5d559e5
Shruti <shrutigawande1910@gmail.com>
2024-05-30 12:51:16 +0530

    fix: UserModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,23 +4,23 @@ const UserSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      require: true,
+      required: true,
     },
     phone: {
       type: String,
-      require: true,
+      required: true,
     },
     email: {
       type: String,
-      require: true,
+      required: true,
     },
     username: {
       type: String,
-      require: true,
+      required: true,
     },
     password: {
       type: String,
-      require: true,
+      required: true,
     },
   },
   {
